4c9e29038abea2ea2bae1e92737bb5892cc854a1
RamiHami <ramil.khamidullin123@gmail.com>
2024-07-01 23:27:16 +0300

    Features: Setup prisma, add new modules auth and user

---
+++ backend/src/auth/auth.controller.ts
@@ -0,0 +1,5 @@
+import { Controller } from '@nestjs/common';
+import { JwtService } from '@nestjs/jwt';
+
+@Controller('auth')
+export class AuthController {}
---
+++ backend/src/auth/constants/jwt.constant.ts
@@ -0,0 +1,1 @@
+export const jwt_key = '?0(8rX2PCT8S';
